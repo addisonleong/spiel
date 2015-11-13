ac6203feb47af4d5efb0d1b56054bf0a01eafd2e
SunMi Lee <sunmee0330@gmail.com>
2015-11-13 14:18:03 -0800

    Initial Commit

---
+++ Spiel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spiel
+//
+//  Created by SunMi Lee on 11/13/15.
+//  Copyright © 2015 SunMi Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
